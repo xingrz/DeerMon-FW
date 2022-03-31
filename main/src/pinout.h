8ed78c532427b82dd9338961560e7aa33d090705
XiNGRZ <hi@xingrz.me>
2022-03-31 23:28:33 +0800

    project: Add pinout

---
+++ main/src/pinout.h
@@ -0,0 +1,15 @@
+#ifndef __PROJECT_PINOUT__
+#define __PROJECT_PINOUT__
+
+#define PIN_BLK 19
+
+#define PIN_BZ_EN 22
+
+#define PIN_LCD_DC 2
+#define PIN_LCD_RST 4
+#define PIN_LCD_SDA 13
+#define PIN_LCD_SCL 14
+
+#define PIN_RMT_RGB 21
+
+#endif  // __PROJECT_PINOUT__
